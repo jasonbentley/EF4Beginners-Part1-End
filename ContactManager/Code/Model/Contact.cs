ad19344ff25853b96c723b7d02c48763dca86c85
Jason Bentley <imsinc@gmail.com>
2018-05-24 06:47:05 -0400

    Initial Check-in

---
+++ ContactManager/Code/Model/Contact.cs
@@ -0,0 +1,12 @@
+﻿namespace ContactManager.Code.Model
+{
+    public class Contact
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string HomePhone { get; set; }
+        public string MobilePhone { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
